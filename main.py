a4841b0b66ffffa7c2149a643d70d3925b9655cd
CarolineDorozhkin <sarahinwinter@gmail.com>
2024-11-08 19:15:46 +0200

    Update main.py

--- main.py
+++ main.py
@@ -18,6 +18,14 @@ def process_form():
     button_discord = request.form.get('button_discord')
     button_html = request.form.get('button_html')
     button_db = request.form.get('button_db')
+
+    email = request.form.get('email')
+    comment = request.form.get('text')
+
+    if email and comment:
+        with open("feedback.txt", "a", encoding="utf-8") as file:
+            file.write(f"Email: {email}\nComment: {comment}\n\n")
+        return redirect('/')
     return render_template('index.html', button_python=button_python, button_discord=button_discord, button_html=button_html, button_db=button_db)
 
 
